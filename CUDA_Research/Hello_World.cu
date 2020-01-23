c8b9c037ed2bb49c84b264a99132ed585387ea60
Justindd1994 <justindd1994@gmail.com>
2020-01-23 23:34:05 +0200

    -Added comments from theory to the practical work.

--- CUDA_Research/Hello_World.cu
+++ CUDA_Research/Hello_World.cu
@@ -12,7 +12,12 @@ int main()
 {
 	int nx = 16, ny = 4;
 
+	//Limit for block size x <= 65536, y <= 2^32-1, z <= 65536
 	dim3 block(8, 2); //Number of Dimension 3 blocks/threads within each grid
+
+	//Limit for grid size x <= 1024, y <= 1024, z <= 64
+	//AND
+	//Limit for grid size x * y * z <= 1024
 	dim3 grid(nx / block.x, ny / block.y); //Number of Dimension 3 grids on the GPU
 
 	hello_cuda <<<grid, block>>>();
